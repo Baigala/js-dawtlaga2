2597ec6e6ba06c5fcace80d4f5a1e370dea9fd01
baigal <osmply@gmail.com>
2022-10-24 16:58:21 +0800

    +

--- app.js
+++ app.js
@@ -1,6 +1,18 @@
-// var, datatype, coersion
+// Onoodriin sedew: git, var, datatype, coersion
 // datatype - 2 torol
 // 1. primitive datatype ==> number, string, boolean, undefined, null
 // 2. non-primitive ==> massiv, object
+// number ogogdliin torol - 10 10,5 -5
+// string ogogdliin torol - "blabla" 'blabla'
+// boolean - true, false
+// undefined - huwisagc anh zarlaj ogoh uyed defaultaar undefined utgatai bn.
+// null - hooson gej temdeglej ogoh.
 
-//
+// variable buyu huwisagc
+// 1. urdaa var || let || const tulhuur ug awna.
+// 2. js tulhuur ugs buyu reserved words -r huwisagciig nerlej bolohgui.
+// 3. huwisagciin ner toogoor ehelj bolohgui
+// 4. CamelCase ashiglan huwisagciin neriig bicih.
+
+// coersion
+// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/Operator_Precedence
